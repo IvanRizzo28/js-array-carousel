8b6bd9a87656e59be69fd0c129aa8a209e28e831
IvanRizzo28 <22ivanrizzo@gmail.com>
2023-02-27 17:54:24 +0100

    fine bonus2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 'use strict';
 
 const images=document.querySelectorAll(".carousel img");
-console.log(images);
+const thumbnails=document.getElementById("thumbnails");
+const thumbnailsImg=document.querySelectorAll(".thumbnails .img");
 let count=0;
 const prev=document.getElementById("prev");
 const next=document.getElementById("next");
 
 prev.addEventListener("click", function(){
     images[count].classList.remove("show");
+    thumbnailsImg[count].classList.remove("show");
     if (count===0) count= images.length-1;
     else count--;
     images[count].classList.add("show");
+    thumbnailsImg[count].classList.add("show");
 });
 
 next.addEventListener("click", function(){
     images[count].classList.remove("show");
+    thumbnailsImg[count].classList.remove("show");
     if (count===images.length-1) count=0;
     else count++;
     images[count].classList.add("show");
+    thumbnailsImg[count].classList.add("show");
 });
